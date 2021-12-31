1098e69a6dc3b96d8fe912cddef05a2b146d02c4
OVG <79023031+ovgtech@users.noreply.github.com>
2021-12-31 19:05:50 +0100

    v2.2

--- tfb.py
+++ tfb.py
@@ -15,12 +15,10 @@ from tfb_secrets import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
 from tfb_drawbar import drawbar as D
 from tfb_milestones_v2 import milestones as M
 
-from tfb_numbers_v2 import current_year as cy
-from tfb_numbers_v2 import half_year as hy
-from tfb_numbers_v2 import previous_day as pd
 from tfb_numbers_v2 import current_day as cd
+from tfb_numbers_v2 import current_year as cy
+from tfb_numbers_v2 import previous_day as pd
 from tfb_numbers_v2 import current_day_upscaled as cd_u
-from tfb_numbers_v2 import total_days as td
 from tfb_numbers_v2 import current_percent_left as cp_l
 
 
@@ -32,8 +30,6 @@ api = tweepy.API(auth)
 
 # Variables.
 cd: str # Current day.
-hy: str # Half of current year.
-td: str # Total days in current year.
 
 cy: str # Current year.
 cd_u: str # Current day upscaled to fit 1:2 Day to Pixel ratio for the drawbar.
@@ -49,13 +45,12 @@ img_remove: str # Name for the previous and now useless image.
 #   - If there is a regular day with no milestone, print the progressbar.
 #   - If a milestone happens this day, print the milestone and the progressbar.
 
-milestones = M(cd, hy, td) # Milestones v2.
+milestones = M(cd) # Milestones v2.
 img_save, img_remove = D(cy, cd_u, cp_l, cd, pd)  # Drawing Progress Bar.
 
-if not milestones:
+if milestones == "None":
     if not img_save:
         pass
-    
     else:
         api.update_with_media(img_save, status="")
         try:
--- tfb_milestones_v2.py
+++ tfb_milestones_v2.py
@@ -8,6 +8,7 @@
 # Imports.
 from tfb_numbers_v2 import half_year as hy
 from tfb_numbers_v2 import total_days as td
+from tfb_numbers_v2 import current_year as cy
 
 # Variables.
 hy: int # Day which is half of the year.
@@ -15,22 +16,25 @@ td: int # Total days in a year.
 mid: str # Half year message.
 last: str # Last day of the year message.
 first: str # First day of the year message.
+message: str # Message to be shown if milestone is happening.
 
 
 # Message intended to show
-mid = "Today we are reaching the 50% of the year."
-last = "Today we are reaching the 100% of the year.\nTomorrow will be a new year!"
-first = "Happy New Year! Everything starts again."
+mid = "Today we reach the halfway point of the year " + str(cy) + ". You still have time."
+last = "Today time is running out for the year " + str(cy) + ". Have you made the most of it?"
+first = "Today everything starts again with " + str(cy) + ". You know what to do."
 
 
 # Function
 def milestones(cd):
-
-    if cd == 1:
-        return first
-    elif cd == hy:
-        return mid
-    elif cd == td:
-        return last
+    if int(cd) == int(1):
+        message = first
+        return message
+    elif int(cd) == int(hy):
+        message = mid
+        return message
+    elif int(cd) == int(td):
+        message = last
+        return message
     else:
         pass
